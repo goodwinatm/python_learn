5e51b1e856126ca1706a9fd91794ae15c5b7c665
yangu <guy52911@hotmail.com>
2023-05-07 15:37:24 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- homework3/homework3/test_homework3.py
+++ homework3/homework3/test_homework3.py
@@ -27,3 +27,14 @@ class TestCreateHero:
         res = hero_mangement.find_hero("jinx")
         assert res == False
 
+    #原有需求发生变化，边界值都增加1（变为最大值100，最小值2）。如何在不改变原始测试数据情况下，还能依然完成对修改需求后的测试（使用fixture）。
+    def test_create_hero_volume_success3(self, get_hero):
+        """
+        边界值以及等价类场景的测试用例
+        """
+        # print(f"英雄AD的血量为{volume}")
+        hero_mangement = HeroManagement()
+        hero_mangement.create_hero("jinx", get_hero, 20)
+        res = hero_mangement.find_hero("jinx")
+        assert res.get("name") == "jinx"
+        assert res.get("volume") == get_hero
