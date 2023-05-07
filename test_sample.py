d0e3335964c95c209471c54a8ba627af57f5a730
yangu <guy52911@hotmail.com>
2023-05-07 00:00:20 -0400

    homework 2

---
+++ test_sample.py
@@ -0,0 +1,21 @@
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(4) == 5
+def test_a():
+    assert True
+
+def test_b():
+    a = 1
+    b = 1
+    c = 2
+    assert a + b == c, f"{a}+{b}=={c}, 结果为真"
+
+def test_c():
+    a = 1
+    b = 1
+    c = 2
+    assert 'abc' in "abcd"
