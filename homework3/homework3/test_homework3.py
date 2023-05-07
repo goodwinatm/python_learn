cbd0656abf9a9fe365fa0cc44ada27bb1e5286d0
yangu <guy52911@hotmail.com>
2023-05-07 14:48:24 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- homework3/homework3/test_homework3.py
+++ homework3/homework3/test_homework3.py
@@ -1,8 +1,10 @@
 import pytest
 from homework2.test_pytest2.hero_management import HeroManagement
+from homework3.homework3.utils import LoadUtils
+
 
 class TestCreateHero:
-    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("volume_data.yaml")["success"])
+    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("hp_data.yaml")["success"])
     def test_create_hero_volume_success(self, volume):
         """
         边界值以及等价类场景的测试用例
@@ -13,3 +15,14 @@ class TestCreateHero:
         res = hero_mangement.find_hero("jinx")
         assert res.get("name") == "jinx"
         assert res.get("volume") == volume
+
+    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("hp_data.yaml")["fail"])
+    def test_create_hero_volume_fail(self, volume):
+        """
+        边界值以及等价类场景的测试用例
+        """
+        # print(f"英雄AD的血量为{volume}")
+        hero_mangement = HeroManagement()
+        hero_mangement.create_hero("jinx", volume, 20)
+        res = hero_mangement.find_hero("jinx")
+        assert res == False
