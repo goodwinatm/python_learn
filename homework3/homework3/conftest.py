0fe4fef077ab71c21d7008f9656481ca6061833e
yangu <guy52911@hotmail.com>
2023-05-07 15:27:56 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- homework3/homework3/conftest.py
+++ homework3/homework3/conftest.py
@@ -1,4 +1,9 @@
 # 创建conftest.py 文件 ，将下面内容添加进去，运行脚本
+import pytest
+
+from homework3.homework3.utils import LoadUtils
+
+
 def pytest_collection_modifyitems(items):
     """
     测试用例收集完成时，将收集到的用例名name和用例标识nodeid的中文信息显示在控制台上
@@ -6,3 +11,12 @@ def pytest_collection_modifyitems(items):
     for i in items:
         i.name=i.name.encode("utf-8").decode("unicode_escape")
         i._nodeid=i.nodeid.encode("utf-8").decode("unicode_escape")
+
+@pytest.fixture(params=LoadUtils.load_yaml("hp_data.yaml")["success"])
+def get_hero(request):
+    new_list = []
+    for i in request.param:
+        i = i + 1
+        new_list.append(i)
+    yield new_list
+    print("测试用例执行完成")
--- homework3/homework3/test_homework3.py
+++ homework3/homework3/test_homework3.py
@@ -4,7 +4,7 @@ from homework3.homework3.utils import LoadUtils
 
 
 class TestCreateHero:
-    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("hp_data.yaml")["success"])
+    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("hp_data.yaml")["success"],ids=["边界值为1", "边界值为2","边界值为98", "边界值为99"])
     def test_create_hero_volume_success(self, volume):
         """
         边界值以及等价类场景的测试用例
@@ -16,7 +16,7 @@ class TestCreateHero:
         assert res.get("name") == "jinx"
         assert res.get("volume") == volume
 
-    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("hp_data.yaml")["fail"])
+    @pytest.mark.parametrize("volume", LoadUtils.load_yaml("hp_data.yaml")["fail"],ids=["无效类字符串", "无效类布尔型","边界值为0", "边界值为100"])
     def test_create_hero_volume_fail(self, volume):
         """
         边界值以及等价类场景的测试用例
@@ -26,3 +26,4 @@ class TestCreateHero:
         hero_mangement.create_hero("jinx", volume, 20)
         res = hero_mangement.find_hero("jinx")
         assert res == False
+
